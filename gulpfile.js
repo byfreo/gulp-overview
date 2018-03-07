b37ddf7c27ed2acb36797538dadd3a57021ca361
Freo <freo@Joans-MacBook-Air.local>
2018-03-08 02:21:44 +0800

    Added Gulp Concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
 
-gulp.task('log', function(){
-	gutil.log('Awesome!!!')
+var jsFiles = ['components/scripts/custom.js'];
+
+gulp.task('js', function(){
+	gulp.src(jsFiles)
+	.pipe(concat('custom.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
