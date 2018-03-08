2528e78c3ff1dbbd203a122be9268ba53d08c2e3
Freo <freo@Joans-MacBook-Air.local>
2018-03-08 18:10:07 +0800

    minify html

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp 				= 	require('gulp'),
 		connect 		= 	require('gulp-connect'),
 		gulpif 			= 	require('gulp-if'),
 		uglify 			= 	require('gulp-uglify'),
+		minifyHTML 			= 	require('gulp-minify-html'),
 		concat 			= 	require('gulp-concat');
 
 // Variables
@@ -57,7 +58,9 @@ gulp.task('compass', function(){
 
 // HTML File + Connect
 gulp.task('html', function(){
-	gulp.src(htmlFiles)
+	gulp.src('builds/development/*.html')
+	.pipe(gulpif(env==='production', minifyHTML()))
+	.pipe(gulpif(env==='production', gulp.dest(outputDir)))
 	.pipe(connect.reload())
 })
 
