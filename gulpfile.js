3b33928b8caa11c420a9bcd7adc61011a018f983
Freo <freo@Joans-MacBook-Air.local>
2018-03-08 10:42:49 +0800

    added jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
+	browserify = require('gulp-browserify'),
 	concat = require('gulp-concat');
+	
 
 var jsFiles = ['components/scripts/custom.js'];
 
 gulp.task('js', function(){
 	gulp.src(jsFiles)
 	.pipe(concat('custom.js'))
+	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
 });
+ 
